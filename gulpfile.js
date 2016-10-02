75bdcae510e5f0a1611d158ce0cb8483479cab6f
Nikita Bredikhin <bredikhin.nikita@gmail.com>
2016-10-02 22:54:03 +0300

    Project structure and gulp setup

--- gulpfile.js
+++ gulpfile.js
@@ -1,51 +1,63 @@
-var gulp = require('gulp');
-var gutil = require('gulp-util');
-var bower = require('bower');
-var concat = require('gulp-concat');
-var sass = require('gulp-sass');
-var minifyCss = require('gulp-minify-css');
-var rename = require('gulp-rename');
-var sh = require('shelljs');
+const gulp = require('gulp');
+const util = require('gulp-util');
+const babel = require('gulp-babel');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
 
-var paths = {
-  sass: ['./scss/**/*.scss']
+const del = require('del');
+const path = require('path');
+
+const BUILD_PATH = './www';
+const SOURCE_PATH = './app/';
+
+const paths = {
+    js: 	 path.join(SOURCE_PATH, '**/*.js'),
+    css:     path.join(SOURCE_PATH, '**/*.css'),
+    html:    path.join(SOURCE_PATH, '**/*.html'),
+    assets:  path.join(SOURCE_PATH, 'assets/**/*'),
+    lib:     path.join(SOURCE_PATH, 'lib/**/*'),
 };
 
-gulp.task('default', ['sass']);
-
-gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .on('error', sass.logError)
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+// Очистка выходной директории
+gulp.task('clean', function (callback) {
+	del.sync(BUILD_PATH, callback);
 });
 
-gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
+// Копирует manifest.json как есть
+gulp.task('ionic', () => gulp.src(path.join(SOURCE_PATH, 'manifest.json')).pipe(gulp.dest(BUILD_PATH)));
+// Копирует папку lib как есть
+gulp.task('lib', () => gulp.src(paths.lib).pipe(gulp.dest(path.join(BUILD_PATH, 'lib'))));
+// Копирует все файлы .html как есть
+gulp.task('html', () => gulp.src(paths.html).pipe(gulp.dest(BUILD_PATH)));
+
+gulp.task('css', () => {
+	gulp.src(paths.css)
+		.pipe(concat('style.css'))
+		.pipe(gulp.dest(BUILD_PATH));
 });
 
-gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+gulp.task('js', () => {
+    let babelPipe = babel({
+        presets: ['es2015']
     });
+    babelPipe.on('error', (e) => {
+    	util.log(util.colors.red(e.name), e.message);
+    	babelPipe.end();
+    });
+    // Игнорируем lib
+    gulp.src([paths.js, '!' + paths.lib])
+    	.pipe(sourcemaps.init())
+    	.pipe(babelPipe)
+    	.pipe(concat('app.js'))
+    	.pipe(sourcemaps.write('.'))
+    	.pipe(gulp.dest(BUILD_PATH));
 });
 
-gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+gulp.task('default', ['clean', 'ionic', 'lib', 'html', 'css', 'js']);
+
+gulp.task('watch', () => {
+	let options = { cwd: './' };
+	gulp.watch([paths.html], options, ['html']);
+	gulp.watch([paths.css], options, ['css']);
+	gulp.watch([paths.js], options, ['js']);
 });
